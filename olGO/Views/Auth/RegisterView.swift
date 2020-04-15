305b25380435165732e7ea378fd1892b4b762fbc
zmeriksen <zmeriksen@icloud.com>
2020-04-14 19:14:16 -0500

    olgo-9: Added SwiftUI signUp Page from Broncosaurus

--- olGO/Views/Auth/RegisterView.swift
+++ olGO/Views/Auth/RegisterView.swift
@@ -1,86 +1,135 @@
-//
-//  RegisterView.swift
-//  olGO
-//
-//  Created by Zach Eriksen on 11/29/19.
-//  Copyright © 2019 oneleif. All rights reserved.
-//
+import SwiftUI
 
-import UIKit
-import SwiftUIKit
-import Combine
-
-class RegisterView: UIView {
-    private var bag = CancelBag()
+// change view title to whatever name is actually going to be
+struct SignUpView: View {
+    @State var username = ""
+    @State var password = ""
+    @State var confirmPassword = ""
     
-    private var email: String = ""
-    private var password: String = ""
-    
-    private var isRequesting: Bool = false
-    
-    init() {
-        super.init(frame: .zero)
-        backgroundColor = .white
-        embed {
-            VStack(distribution: .equalCentering) {
-                [
-                    Spacer(),
-                    VStack {
-                        [
-                            Field(value: "", placeholder: "Email", keyboardType: .emailAddress)
-                                .inputHandler { (value) in
-                                    self.email = value
-                            }
-                            .frame(height: 60),
-                            Field(value: "", placeholder: "Password", keyboardType: .default)
-                                .configure {
-                                    $0.isSecureTextEntry = true
-                            }
-                            .inputHandler { (value) in
-                                self.password = value
-                            }
-                            .frame(height: 60)
-                        ]
-                    },
-                    Button("Register", titleColor: .blue) {
-                        self.register()
-                    }
-                    .frame(height: 60),
-                    Spacer()
-                ]
+    var body: some View {
+        let cornerRadius: CGFloat = 10
+        
+        return VStack {
+            HStack {
+                // Image calls on asset logo, may need to rename if asset name changes
+                Image("oneleifgoodlogo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(cornerRadius)
+                Text("Sign Up for OneLeif!")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
             }
-            .padding()
-        }
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func register() {
-        guard !self.isRequesting else {
-            return
-        }
-        self.isRequesting = true
-        API.instance.register(user: User(email: self.email,
-                                         password: self.password))
-            .sink(receiveCompletion: { (result) in
-                self.isRequesting = false
-                if case .failure(let error) = result {
-                    print(error.localizedDescription)
+            .padding(.top, 10)
+            
+            HStack {
+                Text("Username:")
+                    .padding(.leading, 10)
+                    .foregroundColor(.white)
+                TextField(" \"John\" ", text: $username)
+                    .background(Color.white)
+                    .clipShape( Capsule() )
+            }
+            .padding(.trailing, 10)
+            
+            HStack {
+                Text("Password: ")
+                    .padding(.leading, 10)
+                    .foregroundColor(.white)
+                SecureField(" Check note below", text: $password)
+                    .background(Color.white)
+                    .clipShape( Capsule() )
+            }
+            .padding(.trailing, 10)
+            
+            HStack {
+                Text("Confirm:    ")
+                    .padding(.leading, 10)
+                    .foregroundColor(.white)
+                SecureField(" Re-type Here", text: $confirmPassword)
+                    .background(Color.white)
+                    .clipShape( Capsule() )
+            }
+            .padding(.trailing, 10)
+            .padding(.bottom, 20)
+            
+            Text("Password must have at least 7 characters   ")
+                .padding(.top, 50)
+            
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("At least 1 uppercase")
+                    Text("At least 1 number")
                 }
-            }) { (response) in
-                print(response)
-                
-                DispatchQueue.main.async {
-                    Navigate.shared.go(UIViewController {
-                        LoginView()
-                    }, style: .push)
+                .padding(.trailing, 20)
+                VStack(alignment: .leading){
+                    Text("At least 1 special")
+                    Text("At least 1 lowercase")
                 }
+            }
+            .padding(.bottom, 50)
+            
+            Button("Submit") {
+                self.submit()
+            }.disabled(username.isEmpty || passwordProtocol())
+                .padding(.horizontal, 50)
+                .padding(.vertical)
+                .font(.headline)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .clipShape( Capsule() )
         }
-        .canceled(by: &self.bag)
+        .frame(
+            minWidth: 350,
+            idealWidth: 400,
+            maxWidth: 450,
+            minHeight: 500,
+            idealHeight: 600,
+            maxHeight: 700,
+            alignment: .center)
+            .background( Color.init( white: 0.45))
+            .cornerRadius(cornerRadius)
+            .padding(.horizontal, 10)
+            .padding(.bottom, 100)
     }
-    
+    private func submit() {
+        ///TO DO: add where the username and password are going to be stored and point to that
+        
+    }
+    private func passwordProtocol() -> Bool {
+        var upper = false
+        var lower = false
+        var numb = false
+        var symbol = false
+        
+        var passwordCheck = false
+        
+        for char in password {
+            if char.isUppercase {
+                upper = true
+            } else if char.isLowercase {
+                lower = true
+            } else if char.isNumber {
+                numb = true
+            } else if char.isPunctuation || char.isSymbol {
+                symbol = true
+            }
+            if upper && lower && numb && symbol {
+                passwordCheck = true
+                break
+            }
+        }
+        if passwordCheck && password == confirmPassword {
+            return true
+        }
+        return false
+    }
 }
 
-
+struct SignUpView_Previews: PreviewProvider {
+    static var previews: some View {
+        SignUpView()
+    }
+}
